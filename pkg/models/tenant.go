321712e200fd535afd29ff0c9f20610b0d34cfa5
jsndz <jaisondz9360@gmail.com>
2026-05-27 11:25:01 +0530

    chore:removing tenants

--- pkg/models/tenant.go
+++ pkg/models/tenant.go
@@ -7,25 +7,18 @@ import (
 	"github.com/lib/pq"
 )
 
-
-
 type Policy struct {
-    ID        uuid.UUID `gorm:"type:uuid;default:gen_random_uuid();primaryKey"`
-    Topic     string    `gorm:"size:100;not null"`
-    Channels  pq.StringArray  `gorm:"type:text[];not null"`
-    Locale    string `gorm:"size:100;not null"`
-    CreatedAt time.Time `gorm:"autoCreateTime"`
-
+	ID        uuid.UUID      `gorm:"type:uuid;default:gen_random_uuid();primaryKey"`
+	Topic     string         `gorm:"size:100;not null"`
+	Channels  pq.StringArray `gorm:"type:text[];not null"`
+	Locale    string         `gorm:"size:100;not null"`
+	CreatedAt time.Time      `gorm:"autoCreateTime"`
 }
 
 type IdempotencyKey struct {
-    Key       string    `gorm:"primaryKey;size:64;index:idx_tenant_key"`
-    RequestHash string  `gorm:"not null"`
-    Response   string   `gorm:"type:jsonb"` 
-    StatusCode int      `gorm:"not null"`
-    CreatedAt  time.Time `gorm:"autoCreateTime"`
-
-
+	Key         string    `gorm:"primaryKey;size:64;"`
+	RequestHash string    `gorm:"not null"`
+	Response    string    `gorm:"type:jsonb"`
+	StatusCode  int       `gorm:"not null"`
+	CreatedAt   time.Time `gorm:"autoCreateTime"`
 }
-
-
